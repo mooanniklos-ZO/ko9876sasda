ae19bbcb6334d4fa29277ddcfbed9432a3804050
mooanniklos-ZO <mooanniklos@gmail.com>
2026-08-30 19:04:59 +0300

    fix: add required Material icons and Retrofit Gson converter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.material.icons.extended)
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
@@ -75,6 +76,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
 
     implementation(libs.retrofit)
+    implementation(libs.converter.gson)
     implementation(libs.logging.interceptor)
     implementation(libs.okhttp)
 
